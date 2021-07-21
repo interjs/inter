0feca1e0d3e6e531c79a4ed0a1427f11570d8ded
Denis <76569844+DenisPower1@users.noreply.github.com>
2021-07-21 15:18:43 -0700

    First Inter'lib
     Dymi - The Dynamic CSS loader library for Inter.

---
+++ libs/dymi.js
@@ -0,0 +1,11 @@
+/**
+ * Dymi - The Dynamic CSS loader library for Inter.
+ * Created by - Denis Power.
+ * 2021.
+ * Version - 1.0.0.
+ * https://github.com/DenisPower1/dymi
+ * Released under the MIT lincense.
+ */
+
+
+//UNDER DEVELOPMENT.
