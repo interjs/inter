c88a24b9e7a947757593b86e176c21b989f5ed39
Denis <76569844+DenisPower1@users.noreply.github.com>
2021-09-11 04:02:03 -0700

    The route module
    This file uses the server and variable module.

---
+++ test/route.js
@@ -0,0 +1,58 @@
+ * 
+ *  THIS FILE USES THE (server) AND (variable) MODULES.
+ * 
+ *  CREATED BY DENIS POWER(https://github.com/DenisPower1).
+ * 
+ * 
+ *  2021.
+ * 
+ */
+
+ const Inter=require("./server").Inter;
+ const _var=require("./variable")._var;
+
+
+ Inter.server((re)=>{
+
+      if(re.url=="/"){
+        re.setHeaders={
+            "Content-Type":"text/html"
+        };
+
+       
+      
+          re.sendFile(`./${_var}.html`);
+        return false;
+
+      }
+
+    if(re.url.endsWith(".css") || re.url.endsWith(".js")){
+     
+       // It is either a css or js file.
+
+        re.setHeaders={
+            "Content-Type":"text/plain"
+        };
+
+        re.sendFile(`./${re.url}`);
+
+    }else{
+
+       
+       
+        if(re.url.endsWith(".html")){
+       // It is a html file.
+
+            re.setHeaders={
+            "Content-Type":"text/html"
+        };
+
+
+        re.sendFile(`./${re.url}`);
+    }
+    }
+
+
+ }).host(2000, ()=>{
+     console.log("The server has started!!!")
+ });
