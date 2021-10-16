2f06c415f5105c76fdd6ed6bff94eff5721ad63b
Denis <76569844+DenisPower1@users.noreply.github.com>
2021-10-16 11:05:29 -0700

    Reference performance

---
+++ test/performance/tests/reference.js
@@ -0,0 +1,46 @@
+const inst=new Perf();
+
+// Register performance.
+
+inst.test({
+
+run(){
+  
+toHTML({
+in:"test",
+data:{
+name:"Inter"
+},
+react:"say"
+})
+},
+log(){
+
+console.log("Registered")
+
+}
+
+
+})
+
+// Update performance.
+
+function update(){
+
+inst.test({
+
+run(){
+
+say.name="framework";
+
+},
+
+log(){
+
+console.log("Updated")
+
+}
+
+})
+
+}
