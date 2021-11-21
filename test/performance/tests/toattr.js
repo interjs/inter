3f33ed932b78ce836c35dff935a43adaca72cfeb
Denis <76569844+DenisPower1@users.noreply.github.com>
2021-11-21 11:36:20 -0800

    toATTR()'s performance.

---
+++ test/performance/tests/toattr.js
@@ -0,0 +1,50 @@
+import {Perf} from "./perf.js"
+
+const inst=new Perf();
+
+inst.test({
+
+  run(){
+  // Register performance.
+    
+    toATTR({
+      in:"id",
+      data:{
+      bt:{
+      disabled:null,
+      class:null,
+      title:null,
+      onclick(){
+      
+        
+        
+      }  
+      }
+      }
+    })
+  
+  },
+  log(){
+  
+    console.log("The register is done!")
+    
+  }
+  
+})
+
+inst.test({
+
+  run(){
+  //Update performance.
+    
+    bt.disabled=true;
+    bt.title="Updated!"
+  
+  },
+  log(){
+  
+    console.log("The Update is done!")
+  
+  }
+  
+})
