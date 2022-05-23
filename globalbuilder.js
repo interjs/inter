65a15f5477c5f96565ecba421b2df3c985ea5d74
Denis <76569844+DenisPower1@users.noreply.github.com>
2022-05-23 10:37:21 -0700

    The global Inter builder

---
+++ globalbuilder.js
@@ -0,0 +1,59 @@
+ const fs=require("fs");
+
+ const   __dir="./core2",
+        __files=["helpers.js", "ref.js", "renderif.js", "toattrs.js","template.js", "renderlist.js", "ajax.js"],
+        __exportStatement=/export/g;
+
+        
+    
+let __content=``;
+
+for(const __file of __files){
+
+    __content+=fs.readFileSync(`${__dir}/${__file}`);
+    
+
+};
+
+__content=__content.replace(__exportStatement,"");
+
+
+const __data=`
+(function(){
+    
+/**
+ * Interjs 
+ * MIT LICENSED BY - Denis Power
+ * Repo - https://github.com/interjs/inter
+ * 2021-2022
+ * 
+ * 
+ * 
+ */
+
+ ${__content}
+
+ window.Ref=Ref;
+ window.renderIf=renderIf;
+ window.toAttrs=toAttrs;
+ window.renderList=renderList;
+ window.template=template;
+ window.Backend=Backend;
+
+})();
+
+`
+
+fs.writeFileSync("./inter.js", __data);
+
+console.log("The task was completed succefully.")
+
+
+
+
+
+ 
+
+
+ 
+ 
