08e5e10abda52562ae940db959743b1a11dc704f
DenisPower1 <denispower02@gmail.com>
2025-05-09 14:33:14 +0100

    Add CI/CD Pipelines

---
+++ builderconfig.js
@@ -0,0 +1,24 @@
+export default {
+  version: "2.2.4",
+  year: "2025",
+  files: [
+    "template/errors.js",
+    "renderList/errors.js",
+    "ajax/errors.js",
+    "ref/errors.js",
+    "renderif/errors.js",
+    "toattrs/errors.js",
+    "helpers.js",
+    "ref/index.js",
+    "toattrs/index.js",
+    "renderif/index.js",
+    "template/index.js",
+    "renderList/index.js",
+    "ajax/index.js",
+  ],
+  dir: "./core",
+  types: {
+    dir: "./types",
+    version: "1.0.0",
+  },
+};
